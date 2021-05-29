79f84f5c362cca54d3a21afd74f6385ecdbe3d12
Arpit Manohar Dhore <arpit.dhore8@live.com>
2021-05-29 16:31:37 +0530

    Scripting Started

--- AirplanePhysics/Assets/Airplane Physics/Code/Scripts/Input/Editor/IP_Base_Airplane_Input_Editor.cs
+++ AirplanePhysics/Assets/Airplane Physics/Code/Scripts/Input/Editor/IP_Base_Airplane_Input_Editor.cs
@@ -1,18 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class IP_Base_Airplane_Input_Editor : MonoBehaviour
+
+namespace Dragneel
 {
-    // Start is called before the first frame update
-    void Start()
+    [CustomEditor(typeof(IP_Base_Airplane_Input))]
+    public class IP_Base_Airplane_Input_Editor : Editor
     {
-        
-    }
+        #region Variables
+        private IP_Base_Airplane_Input targetInput;
 
-    // Update is called once per frame
-    void Update()
-    {
+        #endregion
+
+
+
+        #region Bultin Methods
+        private void OnEnable()
+        {
+            targetInput = (IP_Base_Airplane_Input)target;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            string debugInfo = "";
+            debugInfo += "Pitch = " + targetInput.Pitch + "\n";
+            debugInfo += "Roll = " + targetInput.Roll + "\n";
+            debugInfo += "Yaw = " + targetInput.Yaw + "\n";
+            debugInfo += "Throttle = " + targetInput.Throttle + "\n";
+            debugInfo += "Flaps = " + targetInput.Flaps + "\n";
+            debugInfo += "Brake = " + targetInput.Brake + "\n";
+
+
+
+            //Custom Editor Code
+            GUILayout.Space(20);
+            EditorGUILayout.TextArea( debugInfo ,GUILayout.Height(100));
+
+            Repaint();
+
+        }
+        #endregion
         
     }
 }
--- AirplanePhysics/Assets/Airplane Physics/Code/Scripts/Input/IP_Base_Airplane_Input.cs
+++ AirplanePhysics/Assets/Airplane Physics/Code/Scripts/Input/IP_Base_Airplane_Input.cs
@@ -2,17 +2,99 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IP_Base_AirplaneInputs : MonoBehaviour
+namespace Dragneel
 {
-    // Start is called before the first frame update
-    void Start()
+    public class IP_Base_Airplane_Input : MonoBehaviour
     {
-        
-    }
+        #region Variables
+        protected float pitch = 0f;
+        protected float roll = 0f;
+        protected float yaw = 0f;
 
-    // Update is called once per frame
-    void Update()
-    {
+        protected float throttle = 0f;
+        public KeyCode brakekey = KeyCode.Space;
+        protected float brake = 0f;
+
+        public int MaxFlapIncrements = 2;
+        protected int flaps = 0;
+        #endregion
+
+        #region Properties
+        public float Pitch
+            {
+            get { return pitch; }
+            }
         
+        public float Roll
+        {
+            get { return roll; }
+        }
+
+        public float Yaw
+        {
+            get { return yaw; }
+        }
+
+        public float Throttle
+        {
+            get { return throttle; }
+        }
+
+        public int Flaps
+        {
+            get { return flaps; }
+        }
+
+        public float Brake
+        {
+            get { return brake; }
+        }
+
+        #endregion
+
+
+        #region Bultin Methods
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            HandleInput();
+        }
+        #endregion
+
+
+
+        #region Costom Method
+        void HandleInput()
+        {
+            // Input Properties
+            pitch = Input.GetAxis("Vertical");
+            roll = Input.GetAxis("Horizontal");
+            yaw = Input.GetAxis("Yaw");
+            throttle = Input.GetAxis("Throttle");
+
+            // Debug.Log("Pitch:" + pitch + "-" + "Roll" + roll );
+            
+            //process Brake Inputs
+            brake = Input.GetKey(brakekey)? 1f:0f ; 
+            
+            // Process flaps Inputs
+            if(Input.GetKeyDown(KeyCode.F))
+            {
+                flaps += 1;
+            }
+            if(Input.GetKeyDown(KeyCode.G))
+            {
+                flaps -= 1;
+            }
+
+            flaps = Mathf.Clamp(flaps, 0, MaxFlapIncrements);
+        }
+        #endregion
     }
 }
